e147cccaa70a34f282ce71dbe3d665fe313d5b3e
姚祚成 <243522917@qq.com>
2018-06-25 10:21:05 +0800

    reslove android bug

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import { WebView, View, StyleSheet, Platform } from 'react-native';
+import React, {
+  Component
+} from 'react';
+import {
+  WebView,
+  View,
+  StyleSheet,
+  Platform
+} from 'react-native';
 import renderChart from './renderChart';
 import echarts from './echarts.min';
 
+const source = Platform.OS === 'ios' ? require('./tpl.html') : {
+  uri: 'file:///android_asset/tpl.html'
+}
 export default class App extends Component {
 
   constructor(props) {
     super(props);
     this.setNewOption = this.setNewOption.bind(this);
   }
-  
+
 
   componentWillReceiveProps(nextProps) {
-    if(nextProps.option !== this.props.option) {
+    if (nextProps.option !== this.props.option) {
       this.refs.chart.reload();
     }
   }
@@ -33,7 +43,7 @@ export default class App extends Component {
             backgroundColor: this.props.backgroundColor || 'transparent'
           }}
           scalesPageToFit={Platform.OS !== 'ios'}
-          source={require('./tpl.html')}
+          source ={source}
           onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
         />
       </View>
